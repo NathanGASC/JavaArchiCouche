e99da6759186e09bddbef936c91edf1e1cb0cf45
ngasc <nathan.gasc@idakto.com>
2022-06-21 16:52:03 +0200

    add Interface & remove static

--- eu.ensup.gestionTrain.service/src/TrainService.java
+++ eu.ensup.gestionTrain.service/src/TrainService.java
@@ -4,32 +4,34 @@ import java.util.function.Function;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class TrainService {
-    static public String getTrains() throws JsonProcessingException{
+public class TrainService implements ITrainService{
+    ITrainDAO trainDAO = new TrainDAO();
+
+    public String getTrains() throws JsonProcessingException, Error{
         ObjectMapper objectMapper = new ObjectMapper();
-        var trains = objectMapper.writeValueAsString(TrainDAO.get());
+        var trains = objectMapper.writeValueAsString(this.trainDAO.get());
         return trains;
     }
 
-    static public String paintTrain(int id, String color) throws JsonProcessingException{
-        var train = TrainDAO.get(id).get(0);
+    public String paintTrain(int id, String color) throws JsonProcessingException, Error{
+        var train = this.trainDAO.get(id).get(0);
         train.setCouleur(color);
         
         ObjectMapper objectMapper = new ObjectMapper();
-        var trains = objectMapper.writeValueAsString(TrainDAO.update(id, train));
+        var trains = objectMapper.writeValueAsString(this.trainDAO.update(id, train));
 
         return trains;
     }
 
-    static public String createTrain(Train train) throws JsonProcessingException, Error{
-        TrainService.checkVitesse(train);
+    public String createTrain(Train train) throws JsonProcessingException, Error{
+        this.checkVitesse(train);
 
         ObjectMapper objectMapper = new ObjectMapper();
-        var trains = objectMapper.writeValueAsString(TrainDAO.post(train));
+        var trains = objectMapper.writeValueAsString(this.trainDAO.post(train));
         return trains;
     }
 
-    static private void checkVitesse(Train train){
+    private void checkVitesse(Train train){
         if(train.getVitesseMax() < 100){
             throw new Error("Vitesse trop basse");
         }
